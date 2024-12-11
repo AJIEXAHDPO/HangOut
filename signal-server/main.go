e6cd81ffc3b60ee48979ad41e16a6768de983de1
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-12-11 03:25:42 +0300

    add jwt auth

--- signal-server/main.go
+++ signal-server/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"crypto/rsa"
+	"context"
 	"crypto/sha256"
 	"database/sql"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -20,7 +22,8 @@ import (
 
 type User struct {
 	*gorm.Model
-	Hash  [32]byte
+	ID    uint `gorm:"primaryKey"`
+	Hash  string
 	Name  string `gorm:"unique"`
 	Email string `gorm:"unique"`
 }
@@ -29,14 +32,14 @@ type Offer map[string]interface{}
 type Answer map[string]interface{}
 
 type ConnectionMessage struct {
-	TargetID string `json:"targetId"`
+	TargetID uint   `json:"targetId"`
 	Payload  string `json:"payload"`
 	Type     string `json:"type"`
 }
 
 type AuthRequest struct {
 	Name     string `json:"name"`
-	Password string `json:"email"`
+	Password string `json:"password"`
 }
 
 type RegisterRequest struct {
@@ -56,6 +59,7 @@ var upgrader = websocket.Upgrader{
 }
 
 type UserInfo struct {
+	ID    uint   `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
 }
@@ -71,9 +75,21 @@ type UserConnections struct {
 	Connections []*websocket.Conn
 }
 
-var privateKey *rsa.PrivateKey
+// adds cors headers to a response
+func addCorsHeaders(w http.ResponseWriter) {
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+}
 
-var Connections map[string]*UserConnections
+var (
+	privateKey []byte
+	publicKey  []byte
+)
+
+var clients map[uint][]*websocket.Conn
+var mutex sync.Mutex
 
 // Signalling controller.
 // Sends offers and answers to the clients
@@ -90,7 +106,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 			log.Println("failed to read message", err)
 			break
 		}
-		sendTo(buff.TargetID, buff)
+		sendTo(string(buff.TargetID), buff)
 	}
 }
 
@@ -99,28 +115,35 @@ func sendTo(userID string, message ConnectionMessage) {}
 
 // register
 func register(w http.ResponseWriter, r *http.Request) {
+	addCorsHeaders(w)
+
 	// check Content-Type
-	bytes, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "bad request", http.StatusBadRequest)
-	}
-	contentType := http.DetectContentType(bytes)
+	contentType := r.Header.Get("Content-Type")
 	if contentType != "application/json" {
+		log.Println(contentType)
+		log.Println("error", http.StatusUnsupportedMediaType, "Content-Type: application/json is required")
 		http.Error(w, "Content-Type: application/json is required", http.StatusUnsupportedMediaType)
 	}
 
 	decoder := json.NewDecoder(r.Body)
 	var req RegisterRequest
 	if err := decoder.Decode(&req); err != nil {
+		log.Println("error", http.StatusBadRequest, "vaildation failed")
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
 
 	hash := sha256.Sum256([]byte(req.Password))
-	if err := DB.Model(&User{}).Create(User{Name: req.Name, Hash: hash, Email: req.Email}).Error; err != nil {
-		http.Error(w, "bad request", http.StatusBadRequest)
-		return
-	}
+	// if err := DB.Model(&User{}).Create(User{
+	// 	Name:  req.Name,
+	// 	Hash:  hex.EncodeToString(hash[:]),
+	// 	Email: req.Email,
+	// }).Error; err != nil {
+	// 	log.Println("error", http.StatusBadRequest, "failed to create user")
+	// 	http.Error(w, "bad request", http.StatusBadRequest)
+	// 	return
+	// }
+	log.Println("created", hex.EncodeToString(hash[:]))
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Add("Content-Type", "application/json")
 	w.Write([]byte("{\"message\": \"created\"}"))
@@ -128,31 +151,34 @@ func register(w http.ResponseWriter, r *http.Request) {
 
 // auth
 func auth(w http.ResponseWriter, r *http.Request) {
+	addCorsHeaders(w)
+
 	// check Content-Type
-	bytes, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "bad request", http.StatusBadRequest)
-	}
-	contentType := http.DetectContentType(bytes)
+	contentType := r.Header.Get("Content-Type")
 	if contentType != "application/json" {
+		log.Println(contentType)
+		log.Println("error", http.StatusUnsupportedMediaType, "Content-Type: application/json is required")
 		http.Error(w, "Content-Type: application/json is required", http.StatusUnsupportedMediaType)
 	}
 
 	decoder := json.NewDecoder(r.Body)
 	var req AuthRequest
 	if err := decoder.Decode(&req); err != nil {
+		log.Println("error", http.StatusBadRequest, "bad request")
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
 
 	hash := sha256.Sum256([]byte(req.Password))
 	var user User
-	if err := DB.Model(&User{}).First(&user).Where("name = ?", req.Name).Error; err != nil {
+	if err := DB.Model(&User{}).Where("name = ?", req.Name).First(&user).Error; err != nil {
+		log.Println("error", http.StatusForbidden, "authentication failed: no such user with that name")
 		http.Error(w, "authentication failed: no such user with that name", http.StatusForbidden)
 		return
 	}
 
-	if user.Hash != hash {
+	if user.Hash != hex.EncodeToString(hash[:]) {
+		log.Println("error", http.StatusForbidden, hex.EncodeToString(hash[:]), "=", user.Hash)
 		http.Error(w, "authentication failed: incorrect user name or password", http.StatusForbidden)
 		return
 	}
@@ -162,6 +188,7 @@ func auth(w http.ResponseWriter, r *http.Request) {
 
 	token, err := createToken(user)
 	if err != nil {
+		log.Println("error", http.StatusForbidden, "failed to generate token for user, try again", err)
 		http.Error(w, "failed to generate token for user, try again", http.StatusForbidden)
 		return
 	}
@@ -169,6 +196,7 @@ func auth(w http.ResponseWriter, r *http.Request) {
 		"token": token,
 	})
 	if err != nil {
+		log.Println("error", http.StatusForbidden, "failed to marshal json string, try again")
 		http.Error(w, "failed to marshal json string, try again", http.StatusForbidden)
 		return
 	}
@@ -183,40 +211,68 @@ func createToken(user User) (string, error) {
 			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
 		},
 		UserInfo{
+			user.ID,
 			user.Name,
 			user.Email,
 		},
 		"auth",
 	}
-	return token.SignedString(privateKey)
+	privateKeyParsed, err := jwt.ParseRSAPrivateKeyFromPEM(privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	tokenStr, err := token.SignedString(privateKeyParsed)
+	if err != nil {
+		return "", err
+	}
+	return tokenStr, nil
+	// return token.SignedString(privateKey)
 }
 
 // auth middleware
 func checkAuth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		addCorsHeaders(w)
+
 		auth := r.Header.Get("Authorisation")
-		token, found := strings.CutPrefix(auth, "Bearer ")
+		tokenStr, found := strings.CutPrefix(auth, "Bearer ")
 		if !found {
+			log.Println("error", http.StatusForbidden, "no token provided")
 			http.Error(w, "no token provided", http.StatusForbidden)
 			return
 		}
-		if _, err := vaildateToken(token); err != nil {
+		token, err := vaildateToken(tokenStr)
+		if err != nil {
+			log.Println("validation failed:", err)
+			log.Println("error", http.StatusBadRequest, "validation failed")
 			http.Error(w, err.Error(), http.StatusForbidden)
 			return
 		}
 
-		log.Println("auth token is:", token)
+		claims, ok := token.Claims.(*UserClaims)
+		if !ok {
+			log.Println("error", http.StatusForbidden, "Not validid json token")
+			http.Error(w, "Not validid json token", http.StatusForbidden)
+		}
+		log.Println("auth token is:", tokenStr)
+		ctx := context.WithValue(r.Context(), &UserClaims{}, claims)
+		r = r.WithContext(ctx)
 		next.ServeHTTP(w, r)
 	}
 }
 
 // checks if it is the valid token
 func vaildateToken(tokenString string) (*jwt.Token, error) {
+	publicKeyParsed, err := jwt.ParseRSAPrivateKeyFromPEM(publicKey)
+	if err != nil {
+		return nil, err
+	}
 	token, err := jwt.ParseWithClaims(tokenString, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("invalid algorithm")
 		}
-		return privateKey, nil
+		return publicKeyParsed, nil
 	})
 	if err != nil {
 		return nil, err
@@ -231,7 +287,7 @@ func init() {
 	dbuser := "postgres"
 	dbname := "course_work"
 	dbport := "5432"
-	dbpass := "12345aA"
+	dbpass := "postgres"
 	dbConStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow", dbhost, dbuser, dbpass, dbname, dbport)
 	con, err := gorm.Open(postgres.Open(dbConStr))
 	if err != nil {
@@ -244,10 +300,36 @@ func init() {
 	if err := DB.Migrator().AutoMigrate(User{}); err != nil {
 		log.Fatalln("faied to migrate database", err)
 	}
+
+	// @todo
+	// create test user, remove in production
+	var user User
+	hash := sha256.Sum256([]byte("12345aA"))
+	if err := DB.Model(&User{
+		ID: 1,
+	}).Attrs(&User{
+		Name:  "testUser",
+		Email: "alesha.iv03@gmail.com",
+		Hash:  hex.EncodeToString(hash[:]),
+	}).FirstOrCreate(&user).Error; err != nil {
+
+	}
+	DB.Save(&user)
+	// public key
+	publicKey, err = os.ReadFile("./secrets/public_key.pem")
+	if err != nil {
+		log.Fatalln("failed to read pub key")
+	}
+
+	// private key
+	privateKey, err = os.ReadFile("./secrets/private_key.pem")
+	if err != nil {
+		log.Fatalln("failed to read pub key")
+	}
 }
 
 func main() {
-	http.HandleFunc("/ws/{userID}", checkAuth(handleWebSocket))
+	http.HandleFunc("/ws", checkAuth(handleWebSocket))
 	http.HandleFunc("/register", register)
 	http.HandleFunc("/auth", auth)
 	log.Println("server is listening on http://localhost:8080")
